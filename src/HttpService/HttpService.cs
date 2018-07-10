4e8d23274b7f8776efb90c088e297c94fa836186
Jesper Larsson <jesper.x.larsson@scania.com>
2018-07-10 14:32:07 +0200

    Change HttpService to reuse it's HttpClient

--- src/HttpService/HttpService.cs
+++ src/HttpService/HttpService.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using System.Net.Http;
@@ -9,107 +6,75 @@ using Microsoft.AspNetCore.Authentication;
 namespace HttpService
 {
 
-    public class HttpService : IHttpService
-    {
-        private readonly IContextReader _tokenExtractor;
+	public class HttpService : IHttpService
+	{
+		private readonly IContextReader _tokenExtractor;
+		private HttpClient _tracedClient;
+		private HttpClient _client;
 
-        public HttpService(IHttpContextAccessor accessor)
-        {
-            this._tokenExtractor = new HttpContextReader(accessor);
-        }
+		public HttpService(IHttpContextAccessor accessor)
+		{
+			this._tokenExtractor = new HttpContextReader(accessor);
+			_client = new HttpClient();
+		}
 
-        public HttpService(IContextReader tokenExtractor)
-        {
-            _tokenExtractor = tokenExtractor;
-        }
+		public HttpService(IContextReader tokenExtractor)
+		{
+			_tokenExtractor = tokenExtractor;
+		}
 
-        public async Task<HttpResponseMessage> GetAsync(string requestUri, bool passToken = false)
-        {
-            using (var client = new HttpClient())
-            {
-                await EnhanceClientAsync(client, passToken);
+		public async Task<HttpResponseMessage> GetAsync(string requestUri, bool passToken) =>
+			await SendAsync(HttpMethod.Get, requestUri, passToken);
 
-                var response = await client.GetAsync(requestUri);
+		public async Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content, bool passToken) =>
+			await SendAsync(HttpMethod.Post, requestUri, passToken, content);
 
-                return response;
-            }
-        }
+		public async Task<HttpResponseMessage> PutAsync(string requestUri, HttpContent content, bool passToken) =>
+			await SendAsync(HttpMethod.Put, requestUri, passToken, content);
 
-        public async Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content, bool passToken = false)
-        {
-            using (var client = new HttpClient())
-            {
-                await EnhanceClientAsync(client, passToken);
+		public async Task<HttpResponseMessage> DeleteAsync(string requestUri, bool passToken) =>
+			await SendAsync(HttpMethod.Delete, requestUri, passToken);
 
-                var response = await client.PostAsync(requestUri, content);
+		private async Task<HttpResponseMessage> SendAsync(HttpMethod method, string requestUri, bool passToken, HttpContent content = null)
+		{
+			var msg = new HttpRequestMessage(method, requestUri);
+			if (passToken)
+			{
+				var token = await _tokenExtractor.GetTokenAsync();
+				if (!string.IsNullOrEmpty(token))
+				{
+					msg.Headers.Add("Authorization", $"Bearer {token}");
+				}
+			}
 
-                return response;
-            }
-        }
+			if (content != null)
+			{
+				msg.Content = content;
+			}
 
-        public async Task<HttpResponseMessage> PutAsync(string requestUri, HttpContent content, bool passToken)
-        {
-            using (var client = new HttpClient())
-            {
-                await EnhanceClientAsync(client, passToken);
-
-                var response = await client.PutAsync(requestUri, content);
-
-                return response;
-            }
-        }
-
-        public async Task<HttpResponseMessage> DeleteAsync(string requestUri, bool passToken)
-        {
-            using (var client = new HttpClient())
-            {
-                await EnhanceClientAsync(client, passToken);
-
-                var response = await client.DeleteAsync(requestUri);
-
-                return response;
-            }
-        }
-
-        internal async Task EnhanceClientAsync(HttpClient client, bool passToken)
-        {
-            client.DefaultRequestHeaders.TryAddWithoutValidation("X-Correlation-Id", this._tokenExtractor.GetCorrelationId());
-            if (passToken)
-            {
-                var token = await _tokenExtractor.GetTokenAsync();
-                if (!string.IsNullOrEmpty(token))
-                {
-                    client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {token}");
-                }
-            }
-        }
-    }
+			return await _client.SendAsync(msg);
+		}
+	}
 
 
-    public interface IContextReader
-    {
-        Task<string> GetTokenAsync();
-        string GetCorrelationId();
-    }
+	public interface IContextReader
+	{
+		Task<string> GetTokenAsync();
+	}
 
-    public class HttpContextReader : IContextReader
-    {
-        private readonly IHttpContextAccessor _accessor;
-        public HttpContextReader(IHttpContextAccessor accessor)
-        {
-            this._accessor = accessor;
-        }
+	public class HttpContextReader : IContextReader
+	{
+		private readonly IHttpContextAccessor _accessor;
+		public HttpContextReader(IHttpContextAccessor accessor)
+		{
+			this._accessor = accessor;
+		}
 
-        public string GetCorrelationId()
-        {
-            return this._accessor.HttpContext.TraceIdentifier;
-        }
-
-        public async Task<string> GetTokenAsync()
-        {
-            var token = await this._accessor.HttpContext.GetTokenAsync("access_token");
-            return token;
-        }
-    }
+		public async Task<string> GetTokenAsync()
+		{
+			var token = await this._accessor.HttpContext.GetTokenAsync("access_token");
+			return token;
+		}
+	}
 
 }
